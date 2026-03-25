595b9f071aac1d920dfa407216d7a05a795e7fd9
anshuu <154621046+aanshuuu@users.noreply.github.com>
2026-03-25 12:31:47 +0530

    Update path_settings.py

--- codes/path_settings.py
+++ codes/path_settings.py
@@ -6,15 +6,11 @@ This file sets paths to data folders:
 import os
 from pathlib import Path
 
-#DATA_DIR_1 = r"/home/anshu/Documents/Github_push"
-#DATA_DIR_1 = Path(DATA_DIR_1)
-DATA_DIR = r"/home/anshu/Documents/Github_push"
-
+DATA_DIR = r"./Documents"
 DATA_DIR = Path(DATA_DIR)
 
 # set working directory for merging files
-COLLECTION = Path(DATA_DIR / "Data/") # "Original_data/"
+COLLECTION = Path(DATA_DIR / "Data/") 
 
-#RAW = Path(DATA_DIR_1 / "raw_tomogram_mrc_files/")
-#RAW_LAC = Path(DATA_DIR_1 / "LACfactorXraw_tomogram_mrc_files/")
+
 
